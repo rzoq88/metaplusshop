1db55f25205fb1f122a6c4ca156419d4b3c3ec49
rzk <rzk@mac.local>
2023-11-17 20:48:02 +0300

    edit homepage

--- frontend/src/components/Route/Hero/Hero.jsx
+++ frontend/src/components/Route/Hero/Hero.jsx
@@ -15,13 +15,14 @@ const Hero = () => {
         <h1
           className={`text-[35px] leading-[1.2] 800px:text-[60px] text-[#3d3a3a] font-[600] capitalize`}
         >
-           Meta Plus Shop<br /> Best Collection for you
+          
+           Meta Plus Shop<br /> Elevate Your Experience
         </h1>
         <p className="pt-5 text-[16px] font-[Poppins] font-[400] text-[#000000ba]">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Beatae,
-          assumenda? Quisquam itaque <br /> exercitationem labore vel, dolore
-          quidem asperiores, laudantium temporibus soluta optio consequatur{" "}
-          <br /> aliquam deserunt officia. Dolorum saepe nulla provident.
+        At MetaPlus, we believe in more than just transactions; we believe in transformations.<br />
+         Step into a world where innovation meets sophistication, and possibilities are limitless.
+          Welcome to your destination for curated excellence,<br />
+          where each click opens doors to a universe of premium products and unparalleled service.
         </p>
         <Link to="/products" className="inline-block">
             <div className={`${styles.button} mt-5`}>
